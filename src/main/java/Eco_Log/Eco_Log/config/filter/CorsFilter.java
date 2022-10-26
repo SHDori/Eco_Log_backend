94a47793234cb8566161378b34060a72be6a030b
SeungHwan <vw9801@naver.com>
2022-10-26 16:58:28 +0900

    cors필터수정 ACAM ="*" =>각자이름명시

--- src/main/java/Eco_Log/Eco_Log/config/filter/CorsFilter.java
+++ src/main/java/Eco_Log/Eco_Log/config/filter/CorsFilter.java
@@ -25,7 +25,8 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods",
+                "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
