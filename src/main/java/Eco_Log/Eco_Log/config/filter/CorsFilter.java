c1958ed12164dd23d121a519f10eb1d1fe2da69a
SeungHwan <vw9801@naver.com>
2023-01-11 21:25:24 +0900

    cors 주소 2개 오픈

--- src/main/java/Eco_Log/Eco_Log/config/filter/CorsFilter.java
+++ src/main/java/Eco_Log/Eco_Log/config/filter/CorsFilter.java
@@ -29,7 +29,12 @@ public class CorsFilter implements Filter {
         // 배포용
         //response.setHeader("Access-Control-Allow-Origin", "https://peppy-bonbon-ea3a7a.netlify.app");
         // origin두개이상
-        if (origin.endsWith("https://peppy-bonbon-ea3a7a.netlify.app") || origin.endsWith("http://localhost:3000")) {
+//        if (origin.endsWith("https://peppy-bonbon-ea3a7a.netlify.app") || origin.endsWith("http://localhost:3000")) {
+//
+//            response.setHeader("Access-Control-Allow-Origin", origin);
+//        }
+
+        if (origin.endsWith("https://peppy-bonbon-ea3a7a.netlify.app") || origin.endsWith("http://172.30.1.71:3000")|| origin.endsWith("http://192.168.0.155:3000")) {
 
             response.setHeader("Access-Control-Allow-Origin", origin);
         }
