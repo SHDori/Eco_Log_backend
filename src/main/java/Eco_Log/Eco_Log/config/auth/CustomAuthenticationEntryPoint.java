d0e2ad2715c186f381835d12fa515b7f9173636b
SeungHwan <vw9801@naver.com>
2023-02-07 21:32:55 +0900

    토큰 만료에러 분류

--- src/main/java/Eco_Log/Eco_Log/config/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/Eco_Log/Eco_Log/config/auth/CustomAuthenticationEntryPoint.java
@@ -21,33 +21,39 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         System.out.println("exception이 있나?");
         System.out.println(exception);
 
+        // 만료된토큰에 대해서만
         if(exception.equals("토큰이 만료되었습니다.")){
             System.out.println("토큰이 만료되었습니다.");
             errorCode = "토큰이 만료되었습니다.(TokenExpiredException)";
-            setResponse(response,errorCode);
+            setResponse(response,errorCode,0);
         }
 
         else if(exception.equals("잘못된 Signature입니다.")) {
 
             System.out.println("잘못된 Signature입니다. 여기까지오나?");
             errorCode = "잘못된 Signature입니다.(SignatureVerificationException)";
-            setResponse(response, errorCode);
+            setResponse(response, errorCode,1);
         }else if(exception.equals("유효하지 않은 토큰입니다.")) {
             System.out.println("유효하지 않은 토큰입니다.");
             errorCode = "유효하지 않은 토큰입니다.(JWTVerificationException)";
-            setResponse(response, errorCode);
+            setResponse(response, errorCode,2);
         }else if(exception.equals("잘못된 토큰입니다.")) {
             System.out.println("잘못된 토큰입니다.");
             errorCode = "잘못된 토큰입니다.(IllegalArgumentException)";
-            setResponse(response, errorCode);
+            setResponse(response, errorCode,3);
         }
 
 
     }
 
-    private void setResponse(HttpServletResponse response, String errorCode) throws IOException{
+    private void setResponse(HttpServletResponse response, String errorCode,int errorNum) throws IOException{
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        if(errorNum==0){
+            // 토큰 만료일떄만 이렇게 보냄
+            response.setStatus(444);
+        }else{
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
         response.getWriter().println(JwtProperties.HEADER_STRING + " : "+errorCode);
     }
 }
